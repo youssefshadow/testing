3c3048a44e61c90d4edba2e1323810a7fedeec26
youssefshadow <youssef20ben@gmail.com>
2023-05-22 10:12:27 +0200

    testing-start

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Bonjour, monde !");
+
+function calculateSum() {
+  let number1 = parseFloat(document.getElementById("number1").value);
+  let number2 = parseFloat(document.getElementById("number2").value);
+
+  if (isNaN(number1) || isNaN(number2)) {
+    document.querySelector("#result").innerText =
+      "Veuillez saisir des nombres valides.😜";
+  } else {
+    let sum = number1 + number2;
+    document.querySelector("#result").innerText =
+      "Le résultat de l'addition est : " + sum;
+    document.querySelector("#number1").value = "";
+    document.querySelector("#number2").value = "";
+  }
+}
