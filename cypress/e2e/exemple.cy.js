5fd642be46d57df15db34fde2fa8c3eff7002b23
youssefshadow <youssef20ben@gmail.com>
2023-05-22 14:04:52 +0200

    test2

--- cypress/e2e/exemple.cy.js
+++ cypress/e2e/exemple.cy.js
@@ -2,11 +2,22 @@ describe("template spec", () => {
   it("Calcul", () => {
     cy.visit("http://localhost/testing/");
 
-    cy.get("#number1").type("5");
+    cy.get("#number1").type("15");
     cy.get("#number2").type("3");
 
     cy.get("#btn").click();
 
-    cy.get("#result").should("have.text", "Le résultat de l'addition est : 8");
+    cy.get("#result").should("have.text", "Le résultat de l'addition est : 18");
+  });
+
+  it("Champs_vides", () => {
+    cy.visit("http://localhost/testing/");
+
+    cy.get("#btn").click();
+
+    cy.get("#result").should(
+      "have.text",
+      "Veuillez saisir des nombres valides.😜"
+    );
   });
 });
