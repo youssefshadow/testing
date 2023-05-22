c18037f0c77acdcdcdd8dafb7983921acdd130f0
youssefshadow <youssef20ben@gmail.com>
2023-05-22 10:21:05 +0200

    javascript_functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Bonjour, monde !");
-
+//Fonction calcul les deux nombres
 function calculateSum() {
   let number1 = parseFloat(document.getElementById("number1").value);
   let number2 = parseFloat(document.getElementById("number2").value);
